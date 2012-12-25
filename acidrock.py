680b9060ac3df86bd67822073b4b48ac351dc006
C. Flores <flores@petalphile.com>
2012-12-25 11:29:21 -0800

    known kh and ph, larger ranges for kh

--- acidrock.py
+++ acidrock.py
@@ -5,37 +5,33 @@ import math
 from flask import Flask
 
 def co2(kh,ph):
-  return round(3 * kh * 10**(7-ph), 1)
+  return round(3 * kh * 10**( 7 - ph ), 1)
 
 app = Flask(__name__)
 @app.route('/khph')
 
-def khph():
+def kh_ph_range():
   results = []
   dataset = {}
-  for k in range(1, 10):
+  for k in range(10, 100):
+    kh = round(k * 0.1, 1)
     #print "k is %d" % k
     for p in range(50, 80):
       #print p
       ph = round(p * 0.1, 1)
-      dataset['kh'] = k
+      dataset['kh'] = kh
       dataset['ph'] = ph 
-      dataset['co2'] = co2(k,ph)
+      dataset['co2'] = co2(kh,ph)
       results.append(dataset)
       # i suck
       dataset = {}
   return json.dumps(results)
 
-@app.route('/kh/<value>')
+@app.route('/kh/<khvalue>/ph/<phvalue>/')
 
-def fromkh(value):
-  kh = float(value)
-  ph = [round(x * 0.1, 1) for x in range(50, 80)]
-  dataset = {}
-  results = []
-  for p in ph:
-    dataset[p] = co2(kh,p)
-  results.append(dataset)
-  return json.dumps(results)
+def kh_ph_known(khvalue,phvalue):
+  kh = round(float(khvalue),2)
+  ph = round(float(phvalue),2)
+  return co2(kh,ph)
 
 app.run(host='0.0.0.0',debug=True)
