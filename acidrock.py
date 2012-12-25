fb6eec4952c72d7b5a270fb411d8274ea45ceafa
C. Flores <flores@petalphile.com>
2012-12-25 10:48:48 -0800

    get /khph working

--- acidrock.py
+++ acidrock.py
@@ -4,26 +4,32 @@ import json
 import math
 from flask import Flask
 
-def co2(kh,ph)
-  round(3 * kh * 10**(7-ph), 1)
+def co2(kh,ph):
+  return round(3 * kh * 10**(7-ph), 1)
 
 app = Flask(__name__)
 @app.route('/khph')
 
 def khph():
-  ph = [round(x * 0.1, 1) for x in range(50, 80)]
-  kh = range(1, 10)
-
   results = []
   dataset = {}
-  for k in kh:
-    dataset['kh'] = k
-    for p in ph:
-      dataset[p] = co2(k,p)
-    results.append(dataset)
+  for k in range(1, 10):
+    #print "k is %d" % k
+    for p in range(50, 80):
+      #print p
+      ph = round(p * 0.1, 1)
+      dataset['kh'] = k
+      dataset['ph'] = ph 
+      dataset['co2'] = co2(k,ph)
+      results.append(dataset)
+      # i suck
+      dataset = {}
   return json.dumps(results)
 
-@app.route('/kh/<kh>')
+@app.route('/kh/<value>')
+
+def fromkh(value):
+  kh = float(value)
   ph = [round(x * 0.1, 1) for x in range(50, 80)]
   dataset = {}
   results = []
@@ -32,4 +38,4 @@ def khph():
   results.append(dataset)
   return json.dumps(results)
 
-app.run(debug=True)
+app.run(host='0.0.0.0',debug=True)
